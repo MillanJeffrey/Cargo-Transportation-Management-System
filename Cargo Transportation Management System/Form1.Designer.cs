67089a7ffa8534abfcb96bc30dbd0dd75aca1737
Jeffrey Millan <jeffrey_milan@dmc.edu.ph>
2025-12-01 11:13:36 +0800

    Add Project

--- Cargo Transportation Management System/Form1.Designer.cs
+++ Cargo Transportation Management System/Form1.Designer.cs
@@ -28,20 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lognbutton = new System.Windows.Forms.Button();
+            this.userbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passbox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // lognbutton
+            // 
+            this.lognbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lognbutton.Location = new System.Drawing.Point(204, 360);
+            this.lognbutton.Name = "lognbutton";
+            this.lognbutton.Size = new System.Drawing.Size(108, 45);
+            this.lognbutton.TabIndex = 0;
+            this.lognbutton.Text = "Log-in";
+            this.lognbutton.UseVisualStyleBackColor = true;
+            this.lognbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // userbox
+            // 
+            this.userbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userbox.Location = new System.Drawing.Point(140, 246);
+            this.userbox.Name = "userbox";
+            this.userbox.Size = new System.Drawing.Size(248, 22);
+            this.userbox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(137, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // passbox
+            // 
+            this.passbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passbox.Location = new System.Drawing.Point(140, 320);
+            this.passbox.Name = "passbox";
+            this.passbox.Size = new System.Drawing.Size(248, 22);
+            this.passbox.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 473);
+            this.ClientSize = new System.Drawing.Size(537, 473);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userbox);
+            this.Controls.Add(this.lognbutton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button lognbutton;
+        private System.Windows.Forms.TextBox userbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passbox;
     }
 }
 
--- Cargo Transportation Management System/Form1.cs
+++ Cargo Transportation Management System/Form1.cs
@@ -16,5 +16,20 @@ namespace Cargo_Transportation_Management_System
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
